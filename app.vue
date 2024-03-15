8a9bdf2517200db9e8ed122fec56de19aaf95356
steffkes <stefan@gelungen.es>
2024-03-15 10:41:30 +0100

    add contact information

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <NuxtPage></NuxtPage>
+  <footer class="footer mt-6">
+    <div class="content has-text-centered">
+      <p>
+        Hast du Fragen zur Singlebörse?<br />
+        Melde dich bitte bei <strong>Stefan</strong>:
+        <a href="tel:004916097048114">📞 0160 970 48 114</a>&nbsp;
+        <a href="mailto:stefan@mathe.is">✉️ stefan@mathe.is</a>
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script setup>
