1c1ed27257310569648dca51c35730972c7f8e4d
steffkes <stefan@gelungen.es>
2024-07-01 17:31:48 +0200

    refactor homepage into components

---
+++ components/entry.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+    <component :is="past ? 's' : 'p'">
+      <NuxtLink :to="ident">{{ title }}</NuxtLink>
+      <span class="tag is-primary ml-2">🤚 {{ count }}</span>
+    </component>
+  </li>
+</template>
+
+<script setup>
+const { ident, title, past } = defineProps(["ident", "title", "past"]);
+const { data: count } = await useFetch("/api/" + ident + ".json");
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,14 @@
       <div class="content">
         <h1>Singlebörsen</h1>
         <ul>
-          <li>
-            <NuxtLink to="240907-radebeul">07.09. Radebeul</NuxtLink>
-            <span class="tag is-primary ml-2">🤚 {{ count_radebeul }}</span>
-          </li>
-          <li><del>
-            <NuxtLink to="240511-schonach">11.05. Schonach</NuxtLink>
-            <span class="tag is-primary ml-2">🤚 {{ count_schonach }}</span>
-          </del></li>
+          <entry ident="240907-radebeul" title="07.09. Radebeul"></entry>
+          <entry
+            ident="240511-schonach"
+            title="11.05. Schonach"
+            :past="true"
+          ></entry>
         </ul>
       </div>
     </section>
   </div>
 </template>
-
-<script setup>
-const { data: count_radebeul } = await useFetch("/api/240907-radebeul.json");
-const { data: count_schonach } = await useFetch("/api/240511-schonach.json");
-</script>
