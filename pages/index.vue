502f9d000abf446277215d50c38922b5a1c663f1
steffkes <stefan@gelungen.es>
2024-12-17 13:51:40 +0100

    radebeul is already done

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,15 @@
       <div class="content">
         <h1>Singlebörsen</h1>
         <ul>
-          <entry ident="240907-radebeul" title="07.09. Radebeul"></entry>
+          <entry
+            ident="240907-radebeul"
+            title="07.09. Radebeul"
+            past="true"
+          ></entry>
           <entry
             ident="240511-schonach"
             title="11.05. Schonach"
-            :past="true"
+            past="true"
           ></entry>
         </ul>
       </div>
